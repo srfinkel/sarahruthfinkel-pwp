51fb0ea07e96dad1b43b0837ccbc32568c30c1fa
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-22 22:24:26 -0700

    Cleaned up html and css

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,30 +8,34 @@
 		</title>
 	</head>
 	<body>
-			<h1 class="">
-				Purpose, Audience, Goal
+		<header>
+			<h1>
+				Sarah Ruth Finkel PWP: Milestone 1<br>
 			</h1>
+		</header>
 		<main>
-			<p><strong>Purpose: </strong>The purpose of this site is to highlight Sarah Ruth Finkel's skills in coding, website development, ceramic arts, education, and various other mediums.<br>
-			<strong>Audience: </strong>This site is meant to attract audiences that are interested not only in functional but highly creative and beautiful websites. This audience includes professionals in IT, Marketing, Web/App Development, other tech savvy (and not necessarily tech savvy) creatives, and future potential clients with an appreciation of good design.<br>
-			<strong>Goal: </strong>The primary goal of this website is to have a strong presentation of my creative skills in a variety of mediums, have the branding match linked social media channels, and ultimately position me to obtain valued employment and/or clients.</p>
-			<h1>
-				Persona
-			</h1>
-			<p><strong>Name: </strong>Eileen Suits<br>
-			<strong>Age: </strong>35<br>
-			<strong>Profession: </strong>Editor-in-Chief of <em>Creative Tech</em> magazine. She combs the interweb for the newest, freshest websites. She loves highlighting creative web developers on CT's online publication and sharing various "check this out" posts on their social media channels. CT's has a strong following nationally and fairly-decent on a global level, too.<br>
-			<strong>Technology: </strong>Eileen uses Anroid 6 Marshmallow which has multiple apps synchronized with her MacBook Air (when traveling) and MacPro Desktop in the office.<br>
-			<strong>Attitudes and Behaviors: </strong>Eileen needs to constantly be connected to all sources of her communication (SMS, Emails, FB messenger, Google hangouts, etc.). Because she is very busy, she has set up google alerts to help her find new sites/creatives to highlight. She also depends a lot on her social network to send her reccomended features.<br>
-			<strong>Frustrations and Needs: </strong> Eileen sometimes finds it challenging to discover a thoughfully designed website with active and pertinent social media channels to match. Eileen also does not have time to track down creatives that do not respond promptly to her inquires made either on their website or SM channels. Eileen constantly needs to be generating new content for her audience, which includes both tech creatives and seasoned developers.<br>
-			<strong>Goals: </strong> Eileen has a reoccuring goal of identifying at least three new websites to feature at  the beginning of the week and putting them on blast to her large network on Thursdays.</p>
-			<h1>
-				Use Case
-			</h1>
-			<p>Eileen has come across sarahruthfinkel.com and found the website has a clean design <em>and</em> active social media channels. Eileen checks out all the social media channels to get a sense of the web developer's audience and activity. Eileen has chosen sarahruthfinkel.com to be in this week's featured list.
-				Eileen is able to contact the artist, Sarah Ruth Finkel, via the online form on the site and also by sending a DM in twitter. Eileen is able to grab exciting screenshots from the website for her posts and also find interesting tweets to retweet from sarahruthfinkel.com's twitter account.
-				After being featured in CT, sarahruthfinkel.com lands three new clients.
-			</p>
+
+			<strong class="title">Purpose, Audience, Goal</strong>
+
+			<p><strong class="bold">Purpose: </strong>The purpose of this site is to highlight Sarah Ruth Finkel's skills in coding, website development, ceramic arts, education, and various other mediums.</p>
+			<p><strong class="bold">Audience: </strong>This site is meant to attract audiences that are interested not only in functional but highly creative and beautiful websites. This audience includes professionals in IT, Marketing, Web/App Development, other tech savvy (and not necessarily tech savvy) creatives, and future potential clients with an appreciation of good design.</p>
+			<p><strong class="bold">Goal: </strong>The primary goal of this website is to have a strong presentation of my creative skills in a variety of mediums, have the branding match linked social media channels, and ultimately position me to obtain valued employment and/or clients.</p>
+
+			<strong class="title">Persona</strong>
+
+			<p><strong class="bold">Name: </strong>Eileen Suits</p>
+			<p><strong class="bold">Age: </strong>35</p>
+			<p><strong class="bold">Profession: </strong>Editor-in-Chief of <em>Creative Tech</em> magazine. She combs the interweb for the newest, freshest websites. She loves highlighting creative web developers on CT's online publication and sharing various "check this out" posts on their social media channels. CT's has a strong following nationally and fairly-decent on a global level, too.</p>
+			<p><strong class="bold">Technology: </strong>Eileen uses Anroid 6 Marshmallow which has multiple apps synchronized with her MacBook Air (when traveling) and MacPro Desktop in the office.</p>
+			<p><strong class="bold">Attitudes and Behaviors: </strong>Eileen needs to constantly be connected to all sources of her communication (SMS, Emails, FB messenger, Google hangouts, etc.). Because she is very busy, she has set up google alerts to help her find new sites/creatives to highlight. She also depends a lot on her social network to send her reccomended features.</p>
+			<p><strong class="bold">Frustrations and Needs: </strong> Eileen sometimes finds it challenging to discover a thoughfully designed website with active and pertinent social media channels to match. Eileen also does not have time to track down creatives that do not respond promptly to her inquires made either on their website or SM channels. Eileen constantly needs to be generating new content for her audience, which includes both tech creatives and seasoned developers.</p>
+			<p><strong class="bold">Goals: </strong> Eileen has a reoccuring goal of identifying at least three new websites to feature at  the beginning of the week and putting them on blast to her large network on Thursdays.</p>
+
+			<strong class="title">Use Case</strong>
+
+			<p>Eileen has come across <strong class="website">sarahruthfinkel.com</strong> and found the website has a clean design <em>and</em> active social media channels. Eileen checks out all the social media channels to get a sense of the web developer's audience and activity. Eileen chooses <strong class="website">sarahruthfinkel.com</strong> to be in this week's featured list.</p>
+			<p>Eileen is able to contact the artist, Sarah Ruth Finkel, via the online form on the site and also by sending her DM via twitter. Eileen is able to grab exciting screenshots from the website for her posts and also find interesting tweets to retweet from <strong class="website">sarahruthfinkel.com</strong>'s twitter account.</p>
+			<p>After being featured in CT, <strong class="website">sarahruthfinkel.com</strong> lands three new clients.</p>
 		</main>
 	</body>
 </html>
