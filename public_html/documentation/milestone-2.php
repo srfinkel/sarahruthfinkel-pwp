fcda1d48c8cc38a606cd0206304a0848d5fa8464
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-05 08:50:49 -0700

    Added wireframes to milestone 2a

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -17,15 +17,19 @@
 
 			<strong class="title">Content Strategy</strong>
 
-			<p>My site will be one page and consist of three sections.</p>
+			<p>My site will be one page and consist of three sections: Home Page, About Me, and Portfolio & Contact. The overall feel/theme of the website will be <em>movement</em> by incorporating my skills as a ceramic artist and dancer but will focus on my specific skillset as a software developer and entrepreneur.</p>
 
-			<p><strong class="bold">Home Page: </strong>A large portion of the home page will be rotating video montage (optimized for mobile view) of me doing the following movements: throwing on the wheel, screen recordings of completing different design projects, and some dance. A logo of a monkey "keycap" made out of clay with my name "SARU" underneath it with a transparent background approx 72x100 px <a href="http://www.masanyc.com/" class = "website">(see reference here)</a>. The video will not have any sound. A simple text menu will link to the other sections site</p>
+			<p><strong class="bold">Home Page: </strong>A large portion of the home page will be a rotating video montage (optimized for mobile view) of me doing the following movements: throwing on the wheel, screen recordings of completing different design projects, and some dance <a href="http://ayumihorie.com/" class="website">(see reference here)</a>. A logo of a monkey "keycap" made out of clay with my name "SARU" underneath on a transparent background (72x100 px) will be on the video montage <a href="http://www.masanyc.com/" class="website">(see reference here)</a>. The video will not have any sound. A simple text menu will link to the other sections of the site.</p>
 
-			<p><strong class="bold">About Me: </strong></p>
+			<p><strong class="bold">About Me: </strong>This section will start with this quote: <em>"There is a vitality, a life force, an energy, a quickening that is translated through you into action, and because there is only one of you in all of time, this expression is unique. And if you block it, it will never exist through any other medium and it will be lost. The world will not have it." –Martha Graham, dancer and choreographer.</em> In slightly larger text, a brief description of my software developer toolbox will exist here highlighting my proficiency with specific languages and programs. There will also be a link to my business site <a href="http://www.sodafiremedia.com" class="website">Soda Fire Media</a>. I will have one sentence explaining my role as a  ceramicist and contemporary dancer in order to provide some context to my personality and interests.</p>
 
+			<p><strong class="bold">Portfolio & Contact: </strong>This section will include a six image animated, responsive gallery of my website development work. The gallery pop-up will include tools used and my role. I will have a small contact sign up window <a href="http://www.siamusic.net/" class="website">(see reference here)</a>. Upon entering an e-mail address, a content pop-up window will allow the user to add a message. Links to my GitHub, LinkedIn, and Twitter accounts will highlight a consistent brand across all sites.</p>
+
+		<strong class="title">Wireframes</strong>
+
+			<p>This site will be fully responsive on both mobile and desktop screens. These wireframes were created using <a href="https://balsamiq.com/" class="website">balsamiq</a>. The fourth mobile wireframe is an example of the responsive image gallery.</p>
+			<div><img src="../../images/sarahruthfinkel-mobile.png"></div><br>
+			<div><img src="../../images/sarahruthfinkel-desktop.png"></div>
 		</main>
 	</body>
-
-
-
 </html>
