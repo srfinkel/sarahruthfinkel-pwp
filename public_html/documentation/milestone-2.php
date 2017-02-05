f467c702669168ffbd453fb45ef3b4c012792544
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-05 06:46:37 -0700

    Added the content strategy for milestone 2a

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,7 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-	<meta charset="UTF-8">
+		<meta charset="UTF-8" />
+		<link href="styles.css" rel="stylesheet" type="text/css" />
+		<title>
+			Sarah Ruth Finkel
+		</title>
+	</head>
+	<body>
+		<header>
+			<h1>
+				Sarah Ruth Finkel PWP: Milestone 2a
+			</h1>
+		</header>
+		<main>
+
+			<strong class="title">Content Strategy</strong>
+
+			<p>My site will be one page and consist of three sections.</p>
+
+			<p><strong class="bold">Home Page: </strong>A large portion of the home page will be rotating video montage (optimized for mobile view) of me doing the following movements: throwing on the wheel, screen recordings of completing different design projects, and some dance. A logo of a monkey "keycap" made out of clay with my name "SARU" underneath it with a transparent background approx 72x100 px <a href="http://www.masanyc.com/" class = "website">(see reference here)</a>. The video will not have any sound. A simple text menu will link to the other sections site</p>
+
+			<p><strong class="bold">About Me: </strong></p>
+
+		</main>
+	</body>
+
 
 
 </html>
