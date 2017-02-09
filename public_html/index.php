94ed56e4518b08baa1a18e2cb840a2be1303d10d
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-09 16:02:11 -0700

    Started the index.php file; corrected directory structure

---
+++ public_html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<link href="styles/styles.css" rel="stylesheet" type="text/css" />
+		<title>Sarah Ruth Finkel</title>
+	</head>
+	<body>
+		<header>
+
+		</header>
+	</body>
+
+
+</html>
