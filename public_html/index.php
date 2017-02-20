d4c60ab65866b38a2353ce7b46a317731ca5446f
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-19 22:45:56 -0700

    Added new style sheet

--- public_html/index.php
+++ public_html/index.php
@@ -1,14 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8" />
-		<link href="styles/styles.css" rel="stylesheet" type="text/css" />
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+				crossorigin="anonymous"/>
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+				crossorigin="anonymous"/>
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
+				rel="stylesheet"/>
+
+		<link rel="stylesheet" href="styles/live-styles.css" type="text/css"/>
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
 		<title>Sarah Ruth Finkel</title>
 	</head>
 	<body>
 		<header>
-
+			<div class="jumbotron" align="center">
+				<h1>VIDEO PLACEHOLDER</h1>
+				<!--<video>
+					<source src="https://www.youtube.com/embed/M9-hAJ8IrmU"> autoplay="true" muted />
+				</video> -->
+			</div>
+			<div class="container">
+				<div class="row">
+					<nav>
+						<ul class="nav nav-default navbar-static-top nav-justified">
+							<li role="presentation" class="active"><a href="#">Sarah Ruth Finkel</a></li>
+							<li role="presentation"><a href="#">Portfolio & Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
 		</header>
+		<main>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<p>
+							<em>"There is a vitality, a life force, an energy, a quickening that is translated through you into
+								action, and because there is only one of you in all of time, this expression is unique. And if
+								you block it, it will never exist through any other medium and it will be lost. The world will
+								not have it." –Martha Graham, dancer and choreographer.</em>
+						</p>
+					</div>
+				</div>
+			</div>
+		</main>
 	</body>
 
 
