272fa08d1c8ad1fb51e1f29f8c2cd467d2bc618a
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-03-07 15:38:32 -0700

    added contact form

--- public_html/index.php
+++ public_html/index.php
@@ -153,15 +153,71 @@
 					</div>
 				</div>
 			</div>
+			<div class="row"></div>
+			<div class="col-xs-4 col-md-6 col-md-offset-3">
+
+				<!--Begin Contact Form-->
+				<form id="contact-form" action="php/mailer.php" method="post">
+					<div class="form-group">
+						<label for="name">Name <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-user" aria-hidden="true"></i>
+							</div>
+							<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="email">Email <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+							</div>
+							<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="subject">Subject</label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-pencil" aria-hidden="true"></i>
+							</div>
+							<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="message">Message <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-comment" aria-hidden="true"></i>
+							</div>
+							<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
+						</div>
+					</div>
+
+					<!-- reCAPTCHA -->
+					<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+
+					<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+					<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+				</form>
+
+				<!--empty area for form error/success output-->
+				<div class="row">
+					<div class="col-xs-12">
+						<div id="output-area"></div>
+					</div>
+				</div>
+
 			<footer>
 				<div class="container-fluid">
 					<div class="row">
-						<div class="col-xs-12 col-xs-offset-0 col-md-9 col-md-offset-3"">
+						<div class="col-xs-12 col-xs-offset-0 col-md-9 col-md-offset-3">
 							<a href="https://twitter.com/sarahruthfinkel" target="_blank"<button type="button" class="btn btn-link"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></button></a>
 							<a href="https://www.linkedin.com/in/srfinkel" target="_blank" <button type="button" class="btn btn-link"><i class="fa fa-linkedin fa-lg" aria-hidden="true"></i></button></a>
 							<a href="https://github.com/srfinkel" target="_blank"<button type="button" class="btn btn-link"><i class="fa fa-github fa-lg" aria-hidden="true"></i></button></a>
-						<div class="col-xs-12 col-xs-offset-0 col-md-1 col-md-offset-11"
-							<a href="#top" <button type="button" class="btn btn-link"><i class="fa fa-arrow-circle-o-up fa-2x" aria-hidden="true"></i></button></a>
+						<div class="col-xs-12 col-xs-offset-0 col-md-1 col-md-offset-11">
+							<a href="#top" class="scroll" <button type="button" class="btn btn-link"><i class="fa fa-arrow-circle-o-up fa-2x" aria-hidden="true"></i></button></a>
 						</div>
 					</div>
 			</footer>
